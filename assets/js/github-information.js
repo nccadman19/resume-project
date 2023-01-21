a115d83f12d6828387c454c3533a45706c9603ef
Nic <nicolecadman@icloud.com>
2023-01-21 16:15:44 +0000

    images added, github page started

---
+++ assets/js/github-information.js
@@ -0,0 +1,10 @@
+function fetchGithubInformation(event) {
+    var username = $("gh-username").val();
+    if (!username) {
+        $('#gh-user-data').html(`<h2>Please enter a username</h2>`);
+        return;
+    }
+    $('#gh-user-data').html(`
+        <div id="loader"`>
+        <img src="assets/images/")
+}
